9cadea0ec439fb6c2f395125cc374f588a6f9a77
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-19 15:38:52 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppRoutes from './src/routes/App.routes';
+import theme from '@styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <AppRoutes />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,36 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@interfaces": "./src/interfaces",
+            "@helpers": "./src/helpers",
+            "@hooks": "./src/hooks",
+            "@pages": "./src/pages",
+            "@routes": "./src/routes",
+            "@services": "./src/services",
+            "@styles": "./src/styles"
+          },
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json"
+          ],        
+        },
+      ], 
+      ['styled-components'],
+    ],
   };
 };
