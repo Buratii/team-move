6216d416e049e3512e43ec76bab119bc3a09c49d
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-20 00:29:38 -0300

    feat: Create tasks hook

---
+++ src/hooks/task.tsx
@@ -0,0 +1,100 @@
+import { Checklist, Task } from '@interfaces/task.interface';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+type TaskContextData = {
+  addTask: (task: Task) => void;
+  updateChecklist: (index: number, updatedChecklist: Checklist[]) => void;
+  tasks: Task[];
+};
+
+type TaskProviderProps = {
+  children: ReactNode;
+};
+
+const TaskContext = createContext({} as TaskContextData);
+
+function TaskProvider({ children }: TaskProviderProps) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  useEffect(() => {
+    saveTasks();
+  }, [tasks]);
+
+  const saveTasks = async () => {
+    try {
+      await AsyncStorage.setItem('@tasks', JSON.stringify(tasks));
+    } catch (error) {
+      throw Error('Failed to get task.');
+    }
+  };
+
+  const loadTasks = async () => {
+    try {
+      const savedTasks = await AsyncStorage.getItem('@tasks');
+      if (savedTasks) {
+        const parsedTasks = JSON.parse(savedTasks);
+
+        const sortedTasks = parsedTasks.sort((a: Task, b: Task) => {
+          if (a.status === 'incomplete' && b.status === 'complete') {
+            return -1;
+          }
+          if (a.status === 'complete' && b.status === 'incomplete') {
+            return 1;
+          }
+
+          return new Date(a.date) - new Date(b.date);
+        });
+
+        setTasks(sortedTasks);
+      }
+    } catch (error) {
+      console.error('Error loading tasks:', error);
+    }
+  };
+
+  const addTask = (task: Task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const updateChecklist = (index: number, updatedChecklist: Checklist[]) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].checklist = updatedChecklist;
+
+    const validateChecks = updatedChecklist.find(data => data.checked === true);
+
+    if (validateChecks) {
+      updatedTasks[index].status = 'complete';
+    }
+
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <TaskContext.Provider value={{ addTask, updateChecklist, tasks }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
+
+function useTask(): TaskContextData {
+  const context = useContext(TaskContext);
+
+  if (!context) {
+    throw new Error('useTask must be used within an TaskProvider');
+  }
+
+  return context;
+}
+
+export { TaskProvider, useTask };
