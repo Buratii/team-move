268d72e56b168078d252cf8a276f2ed0296a48bf
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-20 00:25:35 -0300

    feat: ad NavigationContainer and AppProvider

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import { StatusBar } from 'expo-status-bar';
 import AppRoutes from './src/routes/App.routes';
 import theme from '@styles/theme';
 import { ThemeProvider } from 'styled-components';
+import AppProvider from '@hooks/index';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="auto" />
       <NavigationContainer>
-        <StatusBar style="auto" />
-        <AppRoutes />
+        <AppProvider>
+          <AppRoutes />
+        </AppProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
