48598a6c76d014491940cedf6c6197c8229158b7
Leonardo Burati <leonardoburati@hotmail.com>
2023-05-20 00:31:05 -0300

    feat: create the app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    action_blue: '#1890FF',
+    success_green: '#34C759',
+    danger_red: '#FF3B30',
+    warning_yellow: '#FFCC00',
+
+    dark_bg: '#2A2837',
+    dark_primary: '#4C4766',
+    dark_secondary: '#38344B',
+    dark_highlight: '#736B99',
+
+    white: '#F5F5F5',
+    placeholder: 'rgba(245, 245, 245, 0.5)',
+  },
+};
